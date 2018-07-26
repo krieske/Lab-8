8cef1ffa9fd5250db786c7aa286fb3da5ebda037
kristen <thehostboy@gmail.com>
2018-07-26 16:51:43 -0400

    Lab 8 updated and completed

--- Lab 8/Program.cs
+++ Lab 8/Program.cs
@@ -23,101 +23,138 @@ namespace Lab_8
             // create array
 
 
-         /*   string[] studentNames = { "person1", "person2", "person3", "person4", "person5" };
-            string[] favoriteFood = { "food1", "food2", "food3", "food4", "food5" };
-            string[] hometown = { "city1", "city2", "city3", "city4", "city5" };
+            string[] studentNames = { "William C", "Samantha", "Patrick", "Chris S", "Zac" };
+            string[] favoriteFood = { "meat in general", "pizza", "hot wings", "spaghetti", "steak" };
+            string[] hometown = { "Detroit City", "Clawson", "Detroit City", "West Bloomfield", "Fayetteville" };
 
             string playAgain = "yes";
 
-            while (playAgain == "yes")
+            while ((playAgain == "yes") || (playAgain == "y"))
             {
                 Console.WriteLine("Please pick a number 1-5 to learn about a student!");
-                string studentNum = Console.ReadLine();
-                studentNum = ValidateUserInput(studentNum);
 
-                int x = int.Parse(studentNum);
-                x = GetNumWithinRange(x);
+
+                int x = GetNumWithinRange();
 
                 Console.WriteLine($"That is {studentNames[x - 1]}! Type 'hometown' or 'favorite food' to learn more!");
-                Console.ReadLine();
+                //string foodOrCity = ValidateFoodCity();
+
+                // Add code to get hometown or fav. food! 
+
+                string food = ValidateFoodCity();
+                if ((food == "food") || (food == "favorite food"))
+                {
+                    Console.WriteLine($"{studentNames[x - 1]}'s favorite food is {favoriteFood[x - 1]}.");
+                }
+                else if ((food == "city") || (food == "hometown"))
+                {
+                    Console.WriteLine($"{studentNames[x - 1]}'s hometown is {hometown[x - 1]}.");
+                }
+
+                Console.WriteLine($"Type 'hometown' or 'favorite food' to learn more!");
+
+                string city = ValidateFoodCity();
+                if ((city == "city") || (city == "hometown"))
+                {
+                    Console.WriteLine($"{studentNames[x - 1]}'s hometown is {hometown[x - 1]}.");
+                }
+                else if ((city == "food") || (city == "favorite food"))
+                {
+                    Console.WriteLine($"{studentNames[x - 1]}'s favorite food is {favoriteFood[x - 1]}.");
+                }
 
 
-                Console.WriteLine("Would you like to learn aboutsomeone else?")
+                Console.WriteLine("Would you like to learn aboutsomeone else(yes/no)?");
+                playAgain = Console.ReadLine();
             }
             Console.WriteLine("Ok! Goodbye!");
             Console.ReadLine();
 
         }
 
-        public static string ValidateUserInput(string input)
+        public static int ValidateUserInput()
         {
 
-            try
+            bool valid = false;
+            int num = 0;
+            while (!valid)
             {
-                int num = int.Parse(input);
-            }
-            catch (FormatException f)
-            {
-                Console.WriteLine("You did not enter a number.");
-                return "0";
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error");
-                return "0";
-            }
+                try
+                {
 
-            return input;
-        } // validates user input, try-catch
+                    num = int.Parse(Console.ReadLine());
+                    valid = true;
+                }
+                catch (FormatException f)
+                {
+                    Console.WriteLine("You did not enter a number.");
 
-        public static int GetNumWithinRange(int num)
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error");
+
+                }
+            }
+            return num;
+        } // validates 1-5 in array w/ exception handling
+        // finished
+
+        public static int GetNumWithinRange()
         {
             int min = 1;
             int max = 5;
 
+            int num = ValidateUserInput();
             while ((num > max) || (num < min))
             {
                 Console.WriteLine("Please pick a number between 1 and 5.");
+                num = ValidateUserInput();
             }
             return num;
+        } // validates the 1-5 int is in range
+        // finished
+
+        public static string ValidateUserString()
+        {
+            bool valid = false;
+            string input = "";
+            while (!valid)
+            {
+                try
+                {
+
+                    input = Console.ReadLine();
+                    valid = true;
+                }
+                catch (FormatException f)
+                {
+                    Console.WriteLine("You did not enter a valid response.");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error");
+
+                }
+            }
+            return input;
+        }// validates input for food/city w/ exception handling
+
+
+        public static string ValidateFoodCity()
+        {
+            string input = Console.ReadLine().ToLower();
+
+            // input = ValidateUserString();
+            while ((input != "food") && (input != "favorite food") && (input != "hometown") && (input != "city"))
+            {
+                Console.WriteLine("Please pick either 'favorite food' or 'hometown'.");
+                input = ValidateUserString();
+            }
+            
+            return input;
         }
-            // finished
-
-            /*public static string ValidateUserString()
-            {
-                string input = Console.ReadLine();
-
-                while (input == null || !Regex.IsMatch(input, @"^\d{1,2}$"))
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                    input = Console.ReadLine();
-                }
-                return input;
-            }// validates user input for food/city
-
-            public static string ValidateStrRange()
-            {
-                string input = Console.ReadLine();
-
-                while (input == null || !Regex.IsMatch(input, @"^\d{1,2}$"))
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                    input = Console.ReadLine();
-                }
-                return input;
-            }// validates a-z range
-
-            public static string ValidateFoodCity()
-            {
-                string input = Console.ReadLine();
-
-                while (input == null || !Regex.IsMatch(input, @"^\d{1,2}$"))
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                    input = Console.ReadLine();
-                }
-                return input;
-            }// needs loop, call validate string to ensure no exceptions thrown, null, etc */
-        }
+    }
 
 }
